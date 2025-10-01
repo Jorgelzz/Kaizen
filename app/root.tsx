aa523bdfae078ca17e23686305baabe19ca16660
jorgelzz <jorgeluiz2027@hotmail.com>
2025-09-30 22:23:49 -0300

    Added new features and routes, dark mode fixed

--- app/root.tsx
+++ app/root.tsx
@@ -41,10 +41,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
-}
-
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
